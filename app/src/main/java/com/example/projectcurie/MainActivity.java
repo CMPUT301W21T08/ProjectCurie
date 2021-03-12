6fbd6a35301dc65021b85ea85ce30570496bbac2
MitchLabrek <labrecqu@ualberta.ca>
2021-03-11 22:06:52 -0700

    changes

--- app/src/main/java/com/example/projectcurie/MainActivity.java
+++ app/src/main/java/com/example/projectcurie/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.projectcurie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button search_exp_btn = findViewById(R.id.search_exp_btn);
+        Button search_person_btn = findViewById(R.id.search_person_btn);
+        Button new_exp_btn = findViewById(R.id.new_exp_btn);
+        Button view_exp_btn = findViewById(R.id.view_exp_btn);
+        Button view_map_btn = findViewById(R.id.view_map_btn);
+        Button barcode_btn = findViewById(R.id.barcode_btn);
+        
     }
+
+    public void viewExperiments() {
+        ///setContentView(R.layout...);
+    }
+    public void viewGeoLocations() {
+        ///setContentView(R.layout...);
+    }
+    public void searchExperiments() {
+        ///setContentView(R.layout...);
+    }
+    public void searchUsers() {
+        ///setContentView(R.layout...);
+    }
+    public void addExperiment() {
+        ///setContentView(R.layout...);
+    }
+
+    public void scanBarcode() {
+        ///setContentView(R.layout...);
+    }
+    public void openSearchUserFragment() {
+        ///setContentView(R.layout...);
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/projectcurie/WelcomeActivity.java
+++ app/src/main/java/com/example/projectcurie/WelcomeActivity.java
@@ -3,6 +3,8 @@ package com.example.projectcurie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
+        Button start_btn = findViewById(R.id.start_button);
+
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goMain();
+            }
+        });
+    }
+    public void goMain() {
+        setContentView(R.layout.activity_main);
+
     }
 }
