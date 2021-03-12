6df61a88c0f957e2b524da8d8f5dddaf27206896
PaulCleofas <paulcleofas10@gmail.com>
2021-03-11 20:51:15 -0700

    created search experiment fragment instead of activity

---
+++ app/src/main/java/com/example/projectcurie/SearchExperimentFragment.java
@@ -0,0 +1,53 @@
+package com.example.projectcurie;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class SearchExperimentFragment extends DialogFragment {
+    private EditText search_keyword;
+    private OnFragmentInteractionListener listener;
+
+    public interface OnFragmentInteractionListener {
+        void goSearchExperiment(String keyword);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener){
+            listener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_search_experiment, null);
+        search_keyword = view.findViewById(R.id.search_keyword_editText);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setView(view)
+                .setTitle("Search Experiments")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String keyword = search_keyword.getText().toString();
+                        listener.goSearchExperiment(keyword);
+                    }}).create();
+    }
+}
