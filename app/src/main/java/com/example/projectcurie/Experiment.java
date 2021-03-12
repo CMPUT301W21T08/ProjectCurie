0b0b858e6c5a212d3d3fc5610a7ebda1535d37b6
PaulCleofas <paulcleofas10@gmail.com>
2021-03-11 19:50:49 -0700

    added logo in welcome screen and added User class

---
+++ app/src/main/java/com/example/projectcurie/Experiment.java
@@ -0,0 +1,4 @@
+package com.example.projectcurie;
+
+public class Experiment {
+}
---
+++ app/src/main/java/com/example/projectcurie/User.java
@@ -0,0 +1,66 @@
+package com.example.projectcurie;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+    private String username;
+    private String email;
+    private String about;
+    private Date dateJoined;
+    private ArrayList<Experiment> ownedExperiments;
+    private ArrayList<String> blacklisted;
+
+    public User(){}
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public Date getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setDateJoined(Date dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
+    public ArrayList<Experiment> getOwnedExperiments() {
+        return ownedExperiments;
+    }
+
+    public ArrayList<String> getBlacklisted(){
+        return blacklisted;
+    }
+    public void removeBlacklisted(String username) {
+        this.blacklisted.remove(username);
+    }
+
+    public void addBlacklist(String username) {
+        this.blacklisted.add(username);
+    }
+}
