98cb867d28f8027d30f6b07c5a170962d09e85cd
PaulCleofas <paulcleofas10@gmail.com>
2021-03-16 12:29:27 -0600

    fixed submit trial activity so it can go back to home

--- app/src/main/java/com/example/projectcurie/SubmitTrialActivity.java
+++ app/src/main/java/com/example/projectcurie/SubmitTrialActivity.java
@@ -1,25 +1,43 @@
 package com.example.projectcurie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import java.io.IOException;
 
 public class SubmitTrialActivity extends AppCompatActivity implements BinomialTrialFragment.BinomialTrialFragmentInteractionListener,
-                                                                        IntegerCountTrialFragment.IntegerCountTrialFragmentInteractionListener{
+                                                                        IntegerCountTrialFragment.IntegerCountTrialFragmentInteractionListener,
+                                                                        CountTrialFragment.CountTrialFragmentInteractionListener{
 
     FrameLayout fragmentLayout;
+    ConstraintLayout submitSuccessLayout;
     Experiment experiment;
+    Button homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_trial);
         fragmentLayout = findViewById(R.id.trialFragmentLayout);
+
+        // Implement home button so the activity would switch to Main Activity
+        homeButton = (Button) findViewById(R.id.submitTrialHomeButton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         /* Grab Data From Intent */
         try {
@@ -49,7 +67,10 @@ public class SubmitTrialActivity extends AppCompatActivity implements BinomialTr
 
     @Override
     public void uploadBinomialTrial(String resultString) {
-
+        //TO DO: Upload result to its respective trial class
+        fragmentLayout.setVisibility(View.GONE);
+        submitSuccessLayout = findViewById(R.id.submitSuccessLayout);
+        submitSuccessLayout.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -59,7 +80,9 @@ public class SubmitTrialActivity extends AppCompatActivity implements BinomialTr
 
     @Override
     public void uploadIntegerCountTrial(String resultString) {
-
+        fragmentLayout.setVisibility(View.GONE);
+        submitSuccessLayout = findViewById(R.id.submitSuccessLayout);
+        submitSuccessLayout.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -67,4 +90,15 @@ public class SubmitTrialActivity extends AppCompatActivity implements BinomialTr
 
     }
 
+    @Override
+    public void uploadCountTrial(String resultString) {
+        fragmentLayout.setVisibility(View.GONE);
+        submitSuccessLayout = findViewById(R.id.submitSuccessLayout);
+        submitSuccessLayout.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void addCountBarcode(String barcodeString) {
+
+    }
 }
