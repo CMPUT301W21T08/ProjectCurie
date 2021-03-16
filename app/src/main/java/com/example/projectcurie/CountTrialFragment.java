dac260e7d996917a5601e997bcf3fae8391fb15c
PaulCleofas <paulcleofas10@gmail.com>
2021-03-16 11:49:06 -0600

    implemented count trial fragment

--- app/src/main/java/com/example/projectcurie/CountTrialFragment.java
+++ app/src/main/java/com/example/projectcurie/CountTrialFragment.java
@@ -1,29 +1,75 @@
 package com.example.projectcurie;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class CountTrialFragment extends Fragment {
-    public CountTrialFragment() {
-        // Required empty public constructor
-    }
-    public static CountTrialFragment newInstance(String param1, String param2) {
-        CountTrialFragment fragment = new CountTrialFragment();
-        return fragment;
+    private String countResult;
+    private TextView result;
+    private Button countButton;
+    private Button addBarcodeButton;
+    private Button generateQRButton;
+    private CountTrialFragment.CountTrialFragmentInteractionListener listener;
+
+    public interface CountTrialFragmentInteractionListener {
+        void uploadCountTrial(String resultString);
+        void addCountBarcode(String barcodeString);
+
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof CountTrialFragment.CountTrialFragmentInteractionListener){
+            listener = (CountTrialFragment.CountTrialFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_count_trial, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        countButton = (Button) view.findViewById(R.id.countTrialButton);
+        generateQRButton = (Button) view.findViewById(R.id.countTrialGenerateQRButton);
+        addBarcodeButton = (Button) view.findViewById(R.id.countTrialSubmitBarcodeButton);
+        EditText barcodeInput = (EditText) view.findViewById(R.id.integerCountTrialBarcodeEditText);
+
+
+        countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.uploadCountTrial("1");
+            }
+        });
+
+
+        addBarcodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String barcode = barcodeInput.toString();
+                listener.addCountBarcode(barcode);
+            }
+        });
+
     }
 }
