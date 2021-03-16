9b75602733013f799ea1c96587d64240b0690c5a
klydep <53328468+klydep@users.noreply.github.com>
2021-03-16 17:04:22 -0600

    User info goes to the database

--- app/src/main/java/com/example/projectcurie/UserProfileActivity.java
+++ app/src/main/java/com/example/projectcurie/UserProfileActivity.java
@@ -2,12 +2,19 @@ package com.example.projectcurie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.IOException;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,8 +25,14 @@ public class UserProfileActivity extends AppCompatActivity {
     TextView emailTextView;
     TextView informationTextView;
     TextView userDateJoin;
-    User user = App.getUser();
+    private User user = App.getUser();
+    String email = user.getUsername() + "@gmail.com";
+    String about = "Tell us something about yourself!";
+
+
     ListView experimentListView;
+    private ExperimentArrayAdapter experimentArrayAdapter;
+    private ArrayList<Experiment> experiments;
 
 
 
@@ -27,6 +40,7 @@ public class UserProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         usernameTextView = findViewById(R.id.username_text);
@@ -35,15 +49,19 @@ public class UserProfileActivity extends AppCompatActivity {
         experimentListView = findViewById(R.id.user_experiment_list);
         userDateJoin = findViewById(R.id.user_join_date);
 
-        /* Setups */
+        /* Initialization of user info */
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference userRef = db.collection("users").document(user.getUsername());
 
-        user.setEmail(user.getUsername()+"@gmail.com");
-        user.setAbout("Tell us something about yourself!");
-        user.setDateJoined(new Date(2021,01,25)); //User join date should be initialized in WelcomeActivity
-
-
-
-
+        /* Updating the email and about of the user in firestore */
+        userRef.update("email", email);
+        userRef.update("about", about);
+        
+        user.setEmail(email);
+        user.setAbout(about);
+        user.setDateJoined(new Date(System.currentTimeMillis())); /* implement in WelcomeActivity when user is new */
+        String dateJoined = user.getDateJoined().toString();
+        userRef.update("dateJoined", dateJoined);
 
         /* Showing the username */
         usernameTextView.setText(user.getUsername());
@@ -55,7 +73,8 @@ public class UserProfileActivity extends AppCompatActivity {
         informationTextView.setText(user.getAbout());
 
         /* Showing the User join date */
-        userDateJoin.setText(user.getDateJoined().toString());
+        userDateJoin.setText(dateJoined);
+
         /* Showing experiments by user */
 
 
