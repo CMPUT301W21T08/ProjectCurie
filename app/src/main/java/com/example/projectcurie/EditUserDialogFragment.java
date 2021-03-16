3c685cee5b0492f24b406179261410de2a742bd6
billsonualberta <billson@ualberta.ca>
2021-03-16 05:06:56 -0600

    Implemented Profile Edit Dialog

--- app/src/main/java/com/example/projectcurie/EditUserDialogFragment.java
+++ app/src/main/java/com/example/projectcurie/EditUserDialogFragment.java
@@ -1,64 +1,92 @@
 package com.example.projectcurie;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditUserDialogFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * This fragment present the user with a number of text fields to update their profile.
+ * @author Joshua Billson
  */
-public class EditUserDialogFragment extends Fragment {
+public class EditUserDialogFragment extends DialogFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditUserCallbackListener listener;
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText aboutEditText;
+    private User user;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EditUserDialogFragment() {
-        // Required empty public constructor
+    public interface EditUserCallbackListener {
+        public void refreshProfile();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditUserDialogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditUserDialogFragment newInstance(String param1, String param2) {
+    public EditUserDialogFragment() {
+    }
+
+    public static EditUserDialogFragment newInstance(User user) {
         EditUserDialogFragment fragment = new EditUserDialogFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        args.putSerializable("user", user);
         return fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof EditUserCallbackListener){
+            listener = (EditUserCallbackListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement EditUserCallbackListener");
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        this.user = (User) getArguments().getSerializable("user");
     }
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_user_dialog, container, false);
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_edit_user_dialog, null);
+        usernameEditText = view.findViewById(R.id.editTextPersonName);
+        emailEditText = view.findViewById(R.id.editTextEmailAddress);
+        aboutEditText = view.findViewById(R.id.editTextDescription);
+
+        usernameEditText.setText(user.getUsername());
+        emailEditText.setText(user.getEmail());
+        aboutEditText.setText(user.getAbout());
+
+        return new AlertDialog.Builder(getContext())
+                .setView(view)
+                .setTitle("Edit User Profile")
+                .setNegativeButton("BACK", null)
+                .setPositiveButton("SUBMIT", (dialog, which) -> {
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    user.setAbout(aboutEditText.getText().toString());
+                    user.setEmail(emailEditText.getText().toString());
+                    db.collection("users").document(user.getUsername())
+                            .set(user)
+                            .addOnSuccessListener(aVoid -> {
+                                App.setUser(user);
+                                listener.refreshProfile();
+                                Toast.makeText(getActivity().getApplicationContext(), "Profile Successfully Updated!", Toast.LENGTH_SHORT);
+                            });
+
+                })
+                .create();
     }
 }
