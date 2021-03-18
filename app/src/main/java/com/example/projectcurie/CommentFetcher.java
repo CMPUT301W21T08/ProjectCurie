79e9eb8c7b1f60f16308b3e3595deb286afba9dd
billsonualberta <billson@ualberta.ca>
2021-03-18 00:42:29 -0600

    Users Can Browse Answers Posted To A Question

--- app/src/main/java/com/example/projectcurie/CommentFetcher.java
+++ app/src/main/java/com/example/projectcurie/CommentFetcher.java
@@ -1,48 +1,57 @@
 package com.example.projectcurie;
 
-import androidx.annotation.Nullable;
 
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class CommentFetcher implements Serializable {
     ArrayList<Comment> comments;
+    private boolean isSet = false;
 
     public CommentFetcher(ArrayList<Comment> comments) {
         this.comments = comments;
     }
 
     public void fetchQuestions(String experiment, Runnable runnable) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("questions").whereEqualTo("experiment", experiment)
-                .addSnapshotListener((value, error) -> {
-                    comments.clear();
-                    for (QueryDocumentSnapshot document : value) {
-                        Comment comment = document.toObject(Comment.class);
-                        comment.setId(document.getId());
-                        comments.add(comment);
-                    }
-                    runnable.run();
-                });
+        if (isSet) {
+            throw new IllegalStateException();
+        } else {
+            isSet = true;
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            db.collection("questions").whereEqualTo("experiment", experiment)
+                    .addSnapshotListener((value, error) -> {
+                        comments.clear();
+                        for (QueryDocumentSnapshot document : value) {
+                            Comment comment = document.toObject(Comment.class);
+                            comment.setId(document.getId());
+                            comments.add(comment);
+                        }
+                        runnable.run();
+                    });
+
+        }
     }
 
-    public void fetchAnswers(Comment question, Runnable runnable) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("questions").document(question.getId()).collection("answers")
-                .get()
-                .addOnSuccessListener(queryDocumentSnapshots -> {
-                    comments.clear();
-                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
-                        comments.add(document.toObject(Comment.class));
-                    }
-                    runnable.run();
-                });
+    public void fetchAnswers(String questionId, Runnable runnable) {
+        if (isSet) {
+            throw new IllegalStateException();
+        } else {
+            isSet = true;
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            db.collection("questions").document(questionId).collection("answers")
+                    .addSnapshotListener((value, error) -> {
+                        comments.clear();
+                        for (QueryDocumentSnapshot document : value) {
+                            Comment comment = document.toObject(Comment.class);
+                            comment.setId(document.getId());
+                            comments.add(comment);
+                        }
+                        runnable.run();
+                    });
+        }
     }
 
     public ArrayList<Comment> getComments() {
